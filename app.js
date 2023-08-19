feb52529c3aa3a0220d5ee16ace11eb83b2fded8
i4Nihon <91135650+i4Nihon@users.noreply.github.com>
2023-08-19 10:26:47 +0200

    Update app.js change express-session to cookie-

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const crypto = require('crypto');
-const  session = require('express-session')
+const  session = require('cookie-session')
 const passport = require("passport");
 const TwitchStrategy = require('passport-twitch').Strategy;
 
