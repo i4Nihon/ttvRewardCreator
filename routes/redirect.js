d6fb47486508a727d50a250c3e2b4085af3a1de7
i4Nihon <mjeczmien2007@gmail.com>
2023-08-26 11:58:41 +0200

    cleaning code p2

--- routes/redirect.js
+++ routes/redirect.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    if (req.query.code) req.session.ttvCode = req.query.code
 
-    if (req.session.sessionAuthenticated === true) {
-        res.redirect('/home');
-    } else {
-        res.render('failure', {title: "Failure", errorCode: "some err in /redirect", TryAgainUrl: process.env.TRY_AGAIN_URL})
-    }
+  if (req.session.sessionAuthenticated === true) {
+    res.redirect('/home');
+  }
+  const params = new  URLSearchParams(req.originalUrl)
+  if (params.has('code')) {
+    req.session.ttvCode = params.get('code')
+    res.redirect('/token')
+  }
 
 })
 
--- routes/token.js
+++ routes/token.js
@@ -3,45 +3,41 @@ const router = express.Router();
 const {exec} = require('child_process')
 require('dotenv').config()
 
-router.get( '/', (req, res) => {
-    let status;
-    let accessTokenLocalVar;
+router.get('/', (req, res) => {
+  let status;
+  let accessTokenLocalVar;
 
-    if (req.session.ttvCode) {
-        const curlGetToken = `curl -X POST https://id.twitch.tv/oauth2/token -d "client_id=${process.env.CLENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.session.ttvCode}&grant_type=authorization_code&redirect_uri=${process.env.REDIRECT}"`
-        exec(curlGetToken, (err, stdout, stderr) => {
-            if (err) return  console.log(err)
-            else if (stderr) return  console.log(stderr)
+  if (req.session.ttvCode) {
+    const curlGetToken = `curl -X POST https://id.twitch.tv/oauth2/token -d "client_id=${process.env.CLENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.session.ttvCode}&grant_type=authorization_code&redirect_uri=${process.env.REDIRECT}"`
+    exec(curlGetToken, (err, stdout, stderr) => {
+      if (err) return console.log(err)
+      else if (stderr) return console.log(stderr)
 
-            accessTokenLocalVar = stdout.accessToken
-            req.session.refreshToken = stdout.refreshToken
-        })
+      accessTokenLocalVar = stdout.accessToken
+      req.session.refreshToken = stdout.refreshToken
+    })
 
-        const curlValidate = `curl -X GET https://id.twitch.tv/oauth2/validate -H "Authorization: Bearer ${accessTokenLocalVar}"`
+    const curlValidate = `curl -X GET https://id.twitch.tv/oauth2/validate -H "Authorization: Bearer ${accessTokenLocalVar}"`
 
-        exec(curlValidate, (err, stdout, stderr) => {
-            if (err) return  console.log(err)
-            else if (stderr) return  console.log(stderr)
+    exec(curlValidate, (err, stdout, stderr) => {
+      if (err) return console.log(err)
+      else if (stderr) return console.log(stderr)
 
-            status = stdout.status
-        })
+      status = stdout.status
+    })
 
-        if (status === 401){
-            res.render('failure', {errorCode: "fail in getToken", title: "failure", TryAgainUrl: process.env.TRY_AGAIN_URL})
-        }
-        if (status === 200){
-            req.session.accessToken = accessTokenLocalVar;
-            res.redirect('/token/redirect')
-        }
+    if (status === 401) {
+      res.render('failure', {errorCode: "fail in getToken", title: "failure", TryAgainUrl: process.env.TRY_AGAIN_URL})
     }
-    else {
-        res.render('failure', {errorCode: "fail in getToken", title: "failure", TryAgainUrl: process.env.TRY_AGAIN_URL})
+    if (status === 200) {
+      req.session.accessToken = accessTokenLocalVar;
+      res.redirect('/redirect')
     }
+  } else {
+    res.render('failure', {errorCode: "fail in getToken", title: "failure", TryAgainUrl: process.env.TRY_AGAIN_URL})
+  }
 
 })
-
-
-
 
 
 module.exports = router;
