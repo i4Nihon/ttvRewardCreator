921401462a340d0625ffe049b39beb549cfb8b30
i4Nihon <91135650+i4Nihon@users.noreply.github.com>
2023-10-12 11:11:22 +0200

    a

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const vars = require('../variables')
+streamersJson = require('../streamersNames.json')
 const {getEditors} = require("../getEditors")
 router.get('/', (req, res) => {
-    let ifEditorFounded = getEditors(vars.streamerId, vars.accessToken, process.env.CLENT_ID, vars.editorName)
+    let ifEditorFounded = getEditors(vars.streamerId, streamersJson[vars.editorName].accessToken, process.env.CLENT_ID, vars.editorName)
     if (ifEditorFounded) {
 
         res.render('home');
